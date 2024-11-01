8200d108991e58cf1cccd4dc264939736cd3d5a1
Yukkurisiteikitai <Yukkuriorsience@gmail.com>
2024-11-01 12:27:03 +0900

    fix:space

--- src/file_manager.c
+++ src/file_manager.c
@@ -4,52 +4,52 @@
 #include <string.h>
 #include <unistd.h>
 
-//書き込み権限があるか
-int check_read_permission(const char* filename) 
+// 読み込み権限があるか
+int check_read_permission(const char* filename)
 {
-    if (access(filename, R_OK) == -1) 
+    if (access(filename, R_OK) == -1)
     {
-        if (errno == EACCES) 
+        if (errno == EACCES)
         {
             fprintf(stderr, "ERROR: '%s' への読み込み権限がありません\n", filename);
             return -1;
         }
-        else if (errno == ENOENT) 
+        else if (errno == ENOENT)
         {
             fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
             return -2;
-        } 
-        else 
+        }
+        else
         {
             fprintf(stderr, "ERROR: %s\n", strerror(errno));
             return -3;
         }
     }
-    // 書き込み権限あり
-    return 0; 
+    // 読み込み権限あり
+    return 0;
 }
 
-//書き込み権限があるか
-int check_write_permission(const char* filename) 
+// 書き込み権限があるか
+int check_write_permission(const char* filename)
 {
-    if (access(filename, W_OK) == -1) 
+    if (access(filename, W_OK) == -1)
     {
-        if (errno == EACCES) 
+        if (errno == EACCES)
         {
             fprintf(stderr, "ERROR:'%s'への書き込み権限がありません\n", filename);
             return -1;
-        } 
-        else if (errno == ENOENT) 
+        }
+        else if (errno == ENOENT)
         {
-            fprintf(stderr, "ERROR: ファイル'%s'が存在しません\n", filename);
+            fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
             return -2;
-        } 
-        else 
+        }
+        else
         {
             fprintf(stderr, "ERROR: %s\n", strerror(errno));
             return -3;
         }
     }
     // 書き込み権限あり
-    return 0; 
+    return 0;
 }
