b9de6f60c1fea5731ef6b18f391f40ed7a4798aa
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-11-01 15:18:08 +0900

    fix conflict

--- src/file_manager.c
+++ src/file_manager.c
@@ -1,85 +1,27 @@
 #include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <string.h>
-#include <unistd.h>
-
-void create_file(const char* filename)
+//ファイルのポインタを返す関数
+FILE* get_file_pointer(const char* filename)
 {
     FILE* file;
-    int status;  // 読み込みできたかのフラグ
-    status = 0;
+
+    //ファイルが存在して正常に開けた時の処理
     file = fopen(filename, "r+");
     if (file != NULL)
     {
-        fclose(file);
-        return;
-    } else
-    {
-        status = 1;
+        return file;
     }
 
-    if (status == 1)
+    //ファイルが存在して新しく作成する処理
+    file = fopen(filename, "a+");
+    if (file != NULL)
     {
-        file = fopen(filename, "w");
-        if (file == NULL)
-        {
-            printf("Failed to create file\n");
-        }
+        return file;
     }
-    fclose(file);
-    return;
-}
-// 読み込み権限があるか
-int check_read_permission(const char* filename)
-{
-    if (access(filename, R_OK) == -1)
-    {
-        if (errno == EACCES)
-        {
-            fprintf(stderr, "ERROR: '%s' への読み込み権限がありません\n",
-                    filename);
 
-            return -1;
-
-        } else if (errno == ENOENT)
-        {
-            fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
-
-            return -2;
-        } else
-        {
-            fprintf(stderr, "ERROR: %s\n", strerror(errno));
-
-            return -3;
-        }
-    }
-    // 読み込み権限あり
-    return 0;
-}
-
-// 書き込み権限があるか
-int check_write_permission(const char* filename)
-{
-    if (access(filename, W_OK) == -1)
-    {
-        if (errno == EACCES)
-        {
-            fprintf(stderr, "ERROR:'%s'への書き込み権限がありません\n",
-                    filename);
-
-            return -1;
-        } else if (errno == ENOENT)
-        {
-            fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
-
-            return -2;
-        } else
-        {
-            fprintf(stderr, "ERROR: %s\n", strerror(errno));
-
-            return -3;
-        }
-    }
-    // 書き込み権限あり
-    return 0;
+    printf("Error: %s", strerror(errno));
+    return NULL;
 }
