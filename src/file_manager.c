17f60b3912bb0cda7e5d94994208c4cf921c1d46
Yukkurisiteikitai <Yukkuriorsience@gmail.com>
2024-11-01 13:24:43 +0900

    fix:format

--- src/file_manager.c
+++ src/file_manager.c
@@ -1,31 +1,28 @@
-#include <errno.h>
-
-#include <stdio.h>
-
-#include <string.h>
-
-#include <unistd.h>
+ #include <errno.h>
+ #include <stdio.h>
+ #include <string.h>
+ #include <unistd.h>
 
 // 読み込み権限があるか
-int check_read_permission(const char* filename)
+ int check_read_permission(const char* filename)
 {
-    if (access(filename,
-        R_OK) == -1)
+    if (access(filename, R_OK) == -1)
     {
         if (errno == EACCES)
         {
-            fprintf(stderr,"ERROR: '%s' への読み込み権限がありません\n",filename);
+            fprintf(stderr, "ERROR: '%s' への読み込み権限がありません\n",
+                    filename);
 
             return -1;
-        
+
         } else if (errno == ENOENT)
         {
-            fprintf(stderr,"ERROR: ファイル '%s' が存在しません\n",filename);
-        
+            fprintf(stderr,"ERROR: ファイル '%s' が存在しません\n", filename);
+
             return -2;
         } else
         {
-            fprintf(stderr,"ERROR: %s\n",strerror(errno));
+            fprintf(stderr,"ERROR: %s\n", strerror(errno));
 
             return -3;
         }
@@ -37,22 +34,21 @@ int check_read_permission(const char* filename)
 // 書き込み権限があるか
 int check_write_permission(const char* filename)
 {
-    if (access(filename,
-        W_OK) == -1)
+    if (access(filename, W_OK) == -1)
     {
         if (errno == EACCES)
         {
-            fprintf(stderr,"ERROR:'%s'への書き込み権限がありません\n",filename);
+            fprintf(stderr, "ERROR:'%s'への書き込み権限がありません\n", filename);
 
             return -1;
         } else if (errno == ENOENT)
         {
-            fprintf(stderr,"ERROR: ファイル '%s' が存在しません\n",filename);
+            fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
 
             return -2;
         } else
         {
-            fprintf(stderr,"ERROR: %s\n",strerror(errno));
+            fprintf(stderr, "ERROR: %s\n", strerror(errno));
 
             return -3;
         }
