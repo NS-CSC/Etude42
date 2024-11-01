b21799f7c3320f49db34498b50d50dcd30de8c33
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-11-01 12:06:00 +0900

    fix format

--- src/file_manager.c
+++ src/file_manager.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 void create_file(const char* filename)
 {
-    FILE *file;
-    int status; //読み込みできたかのフラグ
+    FILE* file;
+    int status;  // 読み込みできたかのフラグ
 
     file = fopen(filename, "r+");
-    if(file == NULL)
+    if (file == NULL)
     {
         status = 1;
     }
-    
-    if(status == 1)
+
+    if (status == 1)
     {
         file = fopen(filename, "w");
-        if(file == NULL)
+        if (file == NULL)
         {
             printf("Failed to create file\n");
         }
     }
-
 }
