fa6cc9b2c70a5e51540760f40583081f53c4db93
Neuroi014 <149709942+Neuroi014@users.noreply.github.com>
2024-11-01 13:01:20 +0900

    Add: コマンドライン引数周りの処理 (#24)
    * main.cの中身を少し拡張しました。
    
    * main.cで、コマンドライン引数を受け取り、オプションとパスを受け取り、それぞれ例外処理を実施するようにしました。
    
    * Fix: フォーマットエラー
    
    * FIx: フォーマットエラー
    
    * Fix: Issueに合わせたmain.cの変更
    
    Issueに合わせてmain.cの内容を変更しました。
    
    * Fix: フォーマットエラーの修正
    
    * Fix: フォーマットエラーの修正
    
    ---------
    
    Co-authored-by: 菊池嶺志 <>

--- main.c
+++ main.c
@@ -1,9 +1,75 @@
 #include <stdio.h>
-#include <unistd.h>
 
-int main(char *argv[])
+// #include "file_manager.h"
+// #include "display.h"
+// #include "input_handler.h"
+// #include "config.h"
+//  これらは他の進捗に応じて変更する。
+
+void start_editor(void);
+
+int main(int argc, char *argv[])
 {
-    const int rc = access(argv[1], 0);
-    printf("rc: %d\n", rc);
+    int commandline_arg_index;
+
+    char *path_name;
+
+    int path_arg_count;
+
+    commandline_arg_index = 1;
+
+    path_arg_count = 0;
+
+    if (argc == 1)
+    {
+        // コマンドライン引数が入力されていない時の条件式
+
+        puts("コマンドライン引数が入力されていません。");
+
+        return 0;
+    }
+
+    while (commandline_arg_index < argc)
+    {
+        if (argv[commandline_arg_index][0] == '-')
+        {
+            // コマンドラインオプションかどうかの区別する条件式
+
+            // if (argv[commandline_arg_index][1] == 'h') {}
+            //  コマンドラインオプション判別の例
+
+            printf("オプション: %s\n", argv[commandline_arg_index]);
+            // 入力されたオプションの確認(テスト)
+        }
+
+        else
+        {
+            // 引数にパスがあったらという条件式
+
+            path_arg_count++;
+
+            if (path_arg_count > 1)
+            {
+                // パスの複数入力があった時の条件式
+
+                puts("パスが複数入力されています。");
+
+                return 0;
+            }
+
+            path_name = argv[1];
+            // path_nameにpathを代入
+
+            // example_function(path_name);
+            //  file_managerへファイルの読み込みを指示
+            //  file_managerから他のファイルの関数を呼び出してエディタが起動する
+
+            printf("path_nameのテスト: %s\n", path_name);
+            // path_nameのテスト
+        }
+
+        commandline_arg_index++;
+    }
+
     return 0;
 }
