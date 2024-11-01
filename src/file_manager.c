6d63f8fea1eddb5b376487e58cc996e9998c30d8
wtnb <144193938+WTNB1225@users.noreply.github.com>
2024-11-01 19:05:13 +0900

    Feature/wtnb1225/add create file (#25)
    * create file
    
    * remove a.exe
    
    * read or create file
    
    * fix format
    
    * fix:format
    
    * fix:return
    
    * fix:format
    
    * Fix: Format
    
    * fix conflict
    
    * fix format
    
    * change variable name
    
    * Update file_manager.c
    
    * Update file_manager.c
    
    ---------
    
    Co-authored-by: mono0218 <81796635+mono0218@users.noreply.github.com>
    Co-authored-by: mono0218 <mono@monokamo.com>

--- src/file_manager.c
+++ src/file_manager.c
@@ -57,3 +57,26 @@ int check_write_permission(const char* filename)
     // 書き込み権限あり
     return 0;
 }
+
+// ファイルのポインタを返す関数
+FILE* get_file_pointer(const char* file_path)
+{
+    FILE* file;
+
+    // ファイルが存在して正常に開けた時の処理
+    file = fopen(file_path, "r+");
+    if (file != NULL)
+    {
+        return file;
+    }
+
+    // ファイルが存在して新しく作成する処理
+    file = fopen(file_path, "a+");
+    if (file != NULL)
+    {
+        return file;
+    }
+
+    printf("Error: %s", strerror(errno));
+    return NULL;
+}
