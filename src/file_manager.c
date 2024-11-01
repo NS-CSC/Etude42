cb975fc1ac3d44d764090e53505be798789f9218
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-11-01 15:24:31 +0900

    fix format

--- src/file_manager.c
+++ src/file_manager.c
@@ -1,21 +1,20 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-//ファイルのポインタを返す関数
+// ファイルのポインタを返す関数
 FILE* get_file_pointer(const char* filename)
 {
     FILE* file;
 
-    //ファイルが存在して正常に開けた時の処理
+    // ファイルが存在して正常に開けた時の処理
     file = fopen(filename, "r+");
     if (file != NULL)
     {
         return file;
     }
 
-    //ファイルが存在して新しく作成する処理
+    // ファイルが存在して新しく作成する処理
     file = fopen(filename, "a+");
     if (file != NULL)
     {
