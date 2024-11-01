a5393a9dc034a79795106336eedd514fab7845b4
Yukkurisiteikitai <Yukkuriorsience@gmail.com>
2024-11-01 13:02:20 +0900

    fix:format

--- src/file_manager.c
+++ src/file_manager.c
@@ -1,6 +1,9 @@
 #include <errno.h>
+
 #include <stdio.h>
+
 #include <string.h>
+
 #include <unistd.h>
 
 // 読み込み権限があるか
@@ -11,10 +14,13 @@ int check_read_permission(const char* filename)
         if (errno == EACCES)
         {
             fprintf(stderr, "ERROR: '%s' への読み込み権限がありません\n", filename);
+
             return -1;
+        
         } else if (errno == ENOENT)
         {
             fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
+        
             return -2;
         } else
         {
@@ -34,14 +40,17 @@ int check_write_permission(const char* filename)
         if (errno == EACCES)
         {
             fprintf(stderr, "ERROR:'%s'への書き込み権限がありません\n", filename);
+
             return -1;
         } else if (errno == ENOENT)
         {
             fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
+
             return -2;
         } else
         {
             fprintf(stderr, "ERROR: %s\n", strerror(errno));
+
             return -3;
         }
     }
