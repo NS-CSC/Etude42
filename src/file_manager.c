9fdf03b36ccf1dc397511854d7c6e3e9510caec1
Yukkurisiteikitai <Yukkuriorsience@gmail.com>
2024-11-01 14:53:23 +0900

    Add: CheckFile Permission (#23)
    * CheckFile_permission
    
    * fix:エラー => ERROR
    
    * fix:allman
    
    * fix:include-abc
    
    googleのコードルールにabc順にモジュールは入れてくださいとあったので変更しました
    
    * fix:printf=>fprintf
    
    * fix:space
    
    * fix:tab=>space
    
    * fix:format
    
    * fix:format
    
    * fix:format
    
    * fix:format
    
    * Update file_manager.c
    
    * fix:format
    
    * fix:format
    
    * fix:format

---
+++ src/file_manager.c
@@ -0,0 +1,59 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+// 読み込み権限があるか
+int check_read_permission(const char* filename)
+{
+    if (access(filename, R_OK) == -1)
+    {
+        if (errno == EACCES)
+        {
+            fprintf(stderr, "ERROR: '%s' への読み込み権限がありません\n",
+                    filename);
+
+            return -1;
+
+        } else if (errno == ENOENT)
+        {
+            fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
+
+            return -2;
+        } else
+        {
+            fprintf(stderr, "ERROR: %s\n", strerror(errno));
+
+            return -3;
+        }
+    }
+    // 読み込み権限あり
+    return 0;
+}
+
+// 書き込み権限があるか
+int check_write_permission(const char* filename)
+{
+    if (access(filename, W_OK) == -1)
+    {
+        if (errno == EACCES)
+        {
+            fprintf(stderr, "ERROR:'%s'への書き込み権限がありません\n",
+                    filename);
+
+            return -1;
+        } else if (errno == ENOENT)
+        {
+            fprintf(stderr, "ERROR: ファイル '%s' が存在しません\n", filename);
+
+            return -2;
+        } else
+        {
+            fprintf(stderr, "ERROR: %s\n", strerror(errno));
+
+            return -3;
+        }
+    }
+    // 書き込み権限あり
+    return 0;
+}
