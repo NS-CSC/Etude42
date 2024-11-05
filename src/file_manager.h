387b9d30d7720472993c860e85cf3685f5ade02b
Neuroi014 <149709942+Neuroi014@users.noreply.github.com>
2024-11-05 10:35:07 +0900

    Fix: file_manager.h
    フォーマットの修正をしました。

--- src/file_manager.h
+++ src/file_manager.h
@@ -5,9 +5,9 @@
 
 FILE *get_file_pointer(const char *file_path);
 // ファイルのポインタを返す関数
-int read_file(const char *file_path)
+int read_file(const char *file_path);
 // ファイルのパスを引数に指定するとファイルを読み込む関数
-int save_file(const char *file_path, const char *data)
+int save_file(const char *file_path, const char *data);
 // ファイルをセーブする関数
 
 #endif // FILE_MANAGER_H
