47c2154bdfa3d124330f1a6c04d12d73d8a2eca9
Neuroi014 <149709942+Neuroi014@users.noreply.github.com>
2024-11-05 21:40:12 +0900

    Fix: file_manager.c
    不要なヘッダの削除

--- src/file_manager.c
+++ src/file_manager.c
@@ -1,9 +1,8 @@
-#include <errno.h>
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include "file_manager.h"
 #include "display.h"
