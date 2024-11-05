6c8675bd95f2c1d7b80aa5c4a90cb22164695ff1
Neuroi014 <>
2024-11-05 22:43:43 +0900

    Add: 途中までファイルの動作の連結をしました

---
+++ src/main.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "config.h"
+#include "display.h"
+#include "file_manager.h"
+#include "input_handler.h"
+
+int main(int argc, char *argv[])
+{
+    // コマンドライン引数の処理をする関数
+
+    int commandline_arg_index;
+    char *path_name;
+    int path_arg_count;
+    int single_dash_option_length;
+
+    path_arg_count = 0;
+
+    if (argc == 1)
+    {
+        // コマンドライン引数が入力されていない時の条件式
+
+        puts("コマンドライン引数が入力されていません。");
+
+        return 0;
+    }
+
+    commandline_arg_index = 1;
+    path_name = NULL;
+
+    while (commandline_arg_index < argc)
+    {
+        single_dash_option_length = 1;
+
+        if (argv[commandline_arg_index][0] == '-')
+        {
+            // コマンドラインオプションかどうかを区別する条件式
+
+            if (argv[commandline_arg_index][1] == '-')
+            {
+                if (strcmp(&argv[commandline_arg_index][2], "help") == 0)
+                {
+                    puts("helpに頼るな");
+                }
+
+                else
+                {
+                    printf("%sは存在しないオプションです\n", argv[commandline_arg_index]);
+                }
+            }
+
+            else
+            {
+                do
+                {
+                    switch (argv[commandline_arg_index][single_dash_option_length])
+                    {
+                        case 'h':
+                            puts("helpに頼るな");
+                            break;
+                        default:
+                            printf("%cは存在しないオプションです\n", argv[commandline_arg_index][single_dash_option_length]);
+                            break;
+                    }
+
+                    single_dash_option_length++;
+                } while (argv[commandline_arg_index][single_dash_option_length] != '\0');
+            }
+        }
+
+        else
+        {
+            // 引数にパスがあったらという条件式
+
+            path_arg_count++;
+
+            if (path_arg_count > 1)
+            {
+                // パスの複数入力があった時の条件式
+
+                puts("パスが複数入力されています。");
+
+                return 0;
+            }
+
+            path_name = argv[commandline_arg_index];
+            // path_nameにpathを代入
+        }
+
+        commandline_arg_index++;
+    }
+
+    if (path_name != NULL)
+    {
+        // pathが存在している時の条件式
+
+        FILE *file_pointer = get_file_pointer(path_name);
+        // 戻り値がFILE型なのでNULL判定できるようにしておく
+        
+        if (file_pointer == NULL)
+        {
+            puts("FILEがNULLです(テスト)");
+
+            return -1;
+        }
+
+        int exit_code = read_file(file_pointer);
+        // 引数エラーが発生している箇所
+        // いつか直す
+
+        if (exit_code == -1)
+        {
+            puts("読み込みエラー(テスト)");
+
+            return -1;
+        }
+
+        //printf("path_name: %s\n", path_name);
+        // path_nameのテスト
+    }
+
+    else
+    {
+        puts("パスが入力されていません");
+        // オプションによって発生する分岐は上で行われるので、ここでパスが入力されていないのはエラーとみなす
+        // なお、ここをelse...ifに変えて別の実装をするのでも良い
+
+        return 0;
+    }
+
+    return 0;
+}
