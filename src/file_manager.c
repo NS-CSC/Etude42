f2138c9fa6da9f2f315b7c65c4ef0321f2788221
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2024-11-08 22:52:04 +0900

    add:get_directory_pointerからポインタを取った後の使い方ガイド

--- src/file_manager.c
+++ src/file_manager.c
@@ -169,3 +169,132 @@ int save_file(const char *file_path, const char *data)
 
     return 0;
 }
+
+/**
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+
+void print_indents(int depth, int branch[])
+{
+    //階層を表しているdepthと縦線を引くかどうかを表すbranchを受け取って、適切なインデントを出力する関数 
+
+    int i;
+    i = 0;
+
+    while (i < depth)
+    {
+        if (branch[i])
+        {
+            printf("│   ");
+        } else
+        {
+            printf("    ");
+        }
+        i++;
+    }
+}
+
+int directory_list(const char* directory_path, int depth, int branch[])
+{
+    //depthは階層を表している mainで呼ぶときは０を指定する branchは縦線を引くかどうかを表す配列
+
+    DIR *dir;
+    struct dirent *dp;
+    char new_path[2049];
+    // directory_pathとdp->d_nameを結合して新しいパスを作るための文字列 長さはそれぞれの最大値が1024, パスを結合するときに/が入るので1を足す 
+    int i = 0;
+    // ループ用の変数
+    int entry_count = 0;
+    //ディレクトリ内の要素の数
+    int entry_index = 0;
+    //ディレクトリ内の要素のインデックス
+
+    dir = get_directory_pointer(directory_path);
+
+    if (dir == NULL)
+    {
+        return -1;
+    }
+    if (depth == 0)
+    {
+        printf(".\n├──\n");
+    }
+
+    while ((dp = readdir(dir)) != NULL)
+    {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
+        //カレントディレクトリや親ディレクトリは子要素ではないのでカウントしない
+        {
+            entry_count++;
+        }
+    }
+
+    rewinddir(dir);
+    //ディレクトリのポインタを先頭に戻す
+
+    while ((dp = readdir(dir)) != NULL)
+    // これは指定されたディレクトリの一つ下の要素を順番に探っていく なくなるのとNULLが返ってくる
+    {
+
+        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+        // カレントディレクトリや親ディレクトリに行くと無限ループになるので回避する
+        {
+            continue;
+        }
+        
+        int is_last = (entry_count - 1 == entry_index);
+        // 最後の要素かどうかを判定する entry_indexは0から始まるので-1する
+
+        print_indents(depth, branch);
+
+        if (is_last)
+        {
+            printf("└── ");
+        } else
+        {
+            printf("├── ");
+        }
+
+        printf("%s\n", dp->d_name);
+
+        entry_index++;
+
+        if (dp->d_type == DT_DIR)
+        //dp->d_typeは要素がディレクトリかファイルかを返す DT_DIRの場合はディレクトリ DT_REGはファイル
+        {
+            snprintf(new_path, sizeof(new_path), "%s/%s", directory_path, dp->d_name);
+            // 現在のディレクトリとその子要素のディレクトリのパスを結合
+            branch[depth] = !is_last;
+            //最後の呼び出しでないなら縦線を残す
+            directory_list(new_path, depth + 1, branch);
+            //階層を1つ深くして再帰的に呼び出す
+        }
+
+    }
+
+    closedir(dir);
+    
+    return 0;
+}
+
+int main(void)
+{
+    int result;
+    result = 0;
+
+    int branch[100] = {0};
+    result = directory_list("/mnt/d/workspace/etude42/", 0, branch);
+    // pathは適宜変更する
+
+    if (result != 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+*/
