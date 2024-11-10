52448c67f9ee02434483fb1d1b673f23de311c5e
Neuroi014 <>
2024-11-10 15:00:37 +0900

    Fix: カーソル移動とスクロールのバグ修正

--- src/input_handler.h
+++ src/input_handler.h
@@ -11,7 +11,7 @@
 
 void input_handler(const int indent_offset, char *file_data[], const int current_max_lines);
 // 別の関数を参照すると手間がかかるため、一時的にショートカットとして使用する関数
-void move_mouse(int *cursor_pos_x, int *cursor_pos_y, const int indent_offset, const int line_len, const unsigned short left_arrow_flag, char *file_data[]);
+void move_mouse(int *cursor_pos_x, int *cursor_pos_y, const int indent_offset, const int line_len, const unsigned short left_arrow_flag, char *file_data[], const int current_scroll);
 // 仮想的なマウスの位置を実際の位置に移動させる関数
 int strlen_utf8(const char *str);
 // マルチバイト文字を含めた文字列の長さを返す関数
